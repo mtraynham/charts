ee6bbd2be3192e00714a8052f927be60b1607d57
Matt Traynham <mrt6467@alum.uncw.edu>
2014-03-15 23:48:55 -0400

    Added example3

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ angular.module('charts').config(['$stateProvider', '$urlRouterProvider', functio
 
     $stateProvider
         .state('example1', { url: '/example1', templateUrl : 'example1/example1.tpl.html', controller : 'Example1Ctrl' })
-        .state('example2', { url: '/example2', templateUrl : 'example2/example2.tpl.html', controller : 'Example2Ctrl' });
+        .state('example2', { url: '/example2', templateUrl : 'example2/example2.tpl.html', controller : 'Example2Ctrl' })
+        .state('example3', { url: '/example3', templateUrl : 'example3/example3.tpl.html', controller : 'Example3Ctrl' });
+
     $urlRouterProvider.otherwise("/example1");
 
 } ]).controller('AppCtrl', [ '$scope', '$state', function($scope, $state) {
