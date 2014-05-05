aa67842dc8975539e9f07d515c91bdde605ada00
Matt Traynham <mrt6467@alum.uncw.edu>
2014-05-05 17:33:56 -0400

    Updates

--- js/example4/example4.js
+++ js/example4/example4.js
@@ -1,3 +1,21 @@
-angular.module('charts').controller('Example4Ctrl', ['$scope', 'GeoConfigs', function ($scope, GeoConfigs) {
+angular.module('charts').controller('Example4Ctrl', ['$scope', 'GeoConfigs', 'GeoProjectionFactory',
+    function ($scope, GeoConfigs, GeoProjectionFactory) {
     $scope.configs = GeoConfigs.getConfigs();
+    $scope.config = GeoConfigs.loadConfig($scope.configs.get('usStates'));
+    $scope.projections = GeoProjectionFactory.types;
+    $scope.projection = GeoProjectionFactory.create();
+
+    $scope.$watch('config', function (newConfig, oldConfig) {
+        if (newConfig === oldConfig) {
+            return;
+        }
+        //_chart.overlayGeoJson
+    });
+
+    $scope.$watch('projection', function (newProjection, oldProjection) {
+        if (newProjection === oldProjection) {
+            return;
+        }
+        // chart.projection
+    });
 }]);
