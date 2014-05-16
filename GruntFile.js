b77451ecbbf6ec0e4231c5bcce9e5ebd31e698a3
Matt Traynham <mrt6467@alum.uncw.edu>
2014-05-15 22:45:44 -0400

    Moved bowercopy config to seperate file

--- GruntFile.js
+++ GruntFile.js
@@ -32,6 +32,7 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
+        bowerfiles: grunt.file.readJSON('bowercopy.json'),
         distdir : 'dist',
         pkg : grunt.file.readJSON('package.json'),
         banner:
@@ -54,100 +55,10 @@ module.exports = function (grunt) {
                 destPrefix: '<%= distdir %>/vendor'
             },
             build: {
-                files: {
-                    // Angular
-                    'angular/angular.js' : 'angular/angular.js',
-                    // Angular Bootstrap
-                    'angular-bootstrap/ui-bootstrap-tpls.js' : 'angular-bootstrap/ui-bootstrap-tpls.js',
-                    // Angular UI Router
-                    'angular-ui-router/release/angular-ui-router.js' :
-                        'angular-ui-router/release/angular-ui-router.js',
-                    // Boostrap
-                    'bootstrap/dist/css/bootstrap.css' : 'bootstrap/dist/css/bootstrap.css',
-                    'bootstrap/dist/fonts' : 'bootstrap/dist/fonts',
-                    // ColorBrewer
-                    'colorbrewer/colorbrewer.js' : 'colorbrewer/colorbrewer.js',
-                    // Crossfilter
-                    'crossfilter/crossfilter.js' : 'crossfilter/crossfilter.js',
-                    // D3
-                    'd3/d3.js' : 'd3/d3.js',
-                    // D3 Geo Projection
-                    'd3-geo-projection/d3.geo.projection.js' : 'd3-geo-projection/d3.geo.projection.js',
-                    // DC
-                    'dc/dc.js' : 'dc/dc.js',
-                    'dc/dc.css' : 'dc/dc.css',
-                    // JQuery
-                    'jquery/dist/jquery.js' : 'jquery/dist/jquery.js',
-                    // JQuery UI
-                    'jquery-ui/ui/jquery-ui.js' : 'jquery-ui/ui/jquery-ui.js',
-                    'jquery-ui/themes/base/jquery.ui.core.css' : 'jquery-ui/themes/base/jquery.ui.core.css',
-                    'jquery-ui/themes/base/jquery.ui.resizable.css' : 'jquery-ui/themes/base/jquery.ui.resizable.css',
-                    // Math JS
-                    'mathjs/dist/math.js' : 'mathjs/dist/math.js',
-                    // Moment JS
-                    'momentjs/moment.js' : 'momentjs/moment.js',
-                    // Queue
-                    'queue-async/queue.js' : 'queue-async/queue.js',
-                    // Sprint F
-                    'sprintf/src/sprintf.js' : 'sprintf/src/sprintf.js',
-                    // TopoJson
-                    'topojson/topojson.js' : 'topojson/topojson.js',
-                    // Topojson Exports
-                    'topojsonexports/world-atlas/topo' : 'topojsonexports/world-atlas/topo',
-                    // Natural Earth Topojson
-                    'natural-earth-topo/topojson' : 'natural-earth-topo/topojson',
-                    // Underscore
-                    'underscore/underscore.js' : 'underscore/underscore.js'
-                }
+                files: '<%= bowerfiles.js %>'
             },
             release: {
-                files: {
-                    // Angular
-                    'angular/angular.js' : 'angular/angular.min.js',
-                    // Angular Bootstrap
-                    'angular-bootstrap/ui-bootstrap-tpls.js' : 'angular-bootstrap/ui-bootstrap-tpls.min.js',
-                    // Angular UI Router
-                    'angular-ui-router/release/angular-ui-router.js' :
-                        'angular-ui-router/release/angular-ui-router.min.js',
-                    // Boostrap
-                    'bootstrap/dist/css/bootstrap.css' : 'bootstrap/dist/css/bootstrap.min.css',
-                    'bootstrap/dist/fonts' : 'bootstrap/dist/fonts',
-                    // ColorBrewer
-                    'colorbrewer/colorbrewer.js' : 'colorbrewer/colorbrewer.js',
-                    // Crossfilter
-                    'crossfilter/crossfilter.js' : 'crossfilter/crossfilter.min.js',
-                    // D3
-                    'd3/d3.js' : 'd3/d3.min.js',
-                    // D3 Geo Projection
-                    'd3-geo-projection/d3.geo.projection.js' : 'd3-geo-projection/d3.geo.projection.min.js',
-                    // DC
-                    'dc/dc.js' : 'dc/dc.min.js',
-                    'dc/dc.css' : 'dc/dc.css',
-                    // JQuery
-                    'jquery/dist/jquery.js' : 'jquery/dist/jquery.min.js',
-                    // JQuery UI
-                    'jquery-ui/ui/jquery-ui.js' : 'jquery-ui/ui/minified/jquery-ui.min.js',
-                    'jquery-ui/themes/base/jquery.ui.core.css' :
-                        'jquery-ui/themes/base/minified/jquery.ui.core.min.css',
-                    'jquery-ui/themes/base/jquery.ui.resizable.css' :
-                        'jquery-ui/themes/base/minified/jquery.ui.resizable.min.css',
-                    // Math JS
-                    'mathjs/dist/math.js' : 'mathjs/dist/math.min.js',
-                    // Moment JS
-                    'momentjs/moment.js' : 'momentjs/min/moment.min.js',
-                    // Queue
-                    'queue-async/queue.js' : 'queue-async/queue.min.js',
-                    // Sprint F
-                    'sprintf/src/sprintf.js' : 'sprintf/src/sprintf.min.js',
-                    // TopoJson
-                    'topojson/topojson.js' : 'topojson/topojson.js',
-                    // Topojson Exports
-                    'topojsonexports/world-atlas/topo' : 'topojsonexports/world-atlas/topo',
-                    // Natural Earth Topojson
-                    'natural-earth-topo/topojson' : 'natural-earth-topo/topojson',
-                    // Underscore
-                    'underscore/underscore.js' : 'underscore/underscore.js'
-                }
+                files: '<%= bowerfiles.min %>'
             }
         },
         clean : {
